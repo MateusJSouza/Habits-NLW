a1311c9538b0c6d3d1a80dd7940ba4f07a536d47
Mateus Jesus <mateusjdev@gmail.com>
2023-01-16 23:18:13 -0300

    feat: configurando prisma e cors

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
+
+// Fastify - framework back-end
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get('/', async () => {
+  const habits = await prisma.habits.findMany();
+
+  return habits;
+});
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server is running on port 3333! 🚀');
+});
