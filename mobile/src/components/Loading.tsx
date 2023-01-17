0a67cab3ebd71d535984085ca423beed6dd1f110
Mateus Jesus <mateusjdev@gmail.com>
2023-01-17 00:38:01 -0300

    feat: assets e componente de loading

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,18 @@
+import { ActivityIndicator, View, StyleSheet } from "react-native";
+
+export function Loading() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator color="#7C3AED"/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#09090A'
+  }
+});
