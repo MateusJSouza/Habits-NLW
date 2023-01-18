93add7116ce68ef533df26d8facae18d2e316dab
Mateus Jesus <mateusjdev@gmail.com>
2023-01-18 16:16:37 -0300

    feat: criação de tabela de hábitos diários

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,34 @@
+import { HabitDay } from './HabitDay';
+
+export function SummaryTable() {
+  const weekDays = [
+    'D',
+    'S',
+    'T',
+    'Q',
+    'Q',
+    'S',
+    'S',
+  ];
+
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        <HabitDay />
+      </div>
+    </div>
+  );
+}
