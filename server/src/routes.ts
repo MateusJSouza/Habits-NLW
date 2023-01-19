3462e1ac8570bca9079146f61e493e77269cf579
Mateus Jesus <mateusjdev@gmail.com>
2023-01-19 16:02:48 -0300

    feat: rota de completar hábitos

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -67,9 +67,72 @@ export async function appRoutes(app: FastifyInstance) {
       return dayHabit.habit_id;
     });
 
+    console.log({ completedHabits });
+
     return {
       possibleHabits,
       completedHabits
     };
+  });
+
+  // Completar / não completar um hábito
+  app.patch('/habits/:id/toggle', async (request) => {
+    // route param => paraâmetro de identificação
+
+    const toggleHabitParams = z.object({
+      id: z.string().uuid()
+    });
+
+    const { id } = toggleHabitParams.parse(request.params);
+
+    const today = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      }
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        }
+      });
+    }
+
+    // Verificando se o usuário já tinha marcado o hábito como completo nesse dia
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      }
+    });
+
+    if (dayHabit) {
+      // remove a marcação de completo
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        }
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        }
+      });
+    }
+
+    // Completar o hábito
+    await prisma.dayHabit.create({
+      data: {
+        day_id: day.id,
+        habit_id: id
+      }
+    });
   });
 }
