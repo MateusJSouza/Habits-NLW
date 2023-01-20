3d47f30d29581aab4abe9edc8fa0a617123afe81
Mateus Jesus <mateusjdev@gmail.com>
2023-01-20 12:46:07 -0300

    feat: input de novo hábito

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,5 +1,6 @@
-import { ScrollView, View } from 'react-native';
+import { ScrollView, Text, TextInput, View } from 'react-native';
 import { BackButton } from '../components/BackButton';
+import colors from 'tailwindcss/colors';
 
 export function New() {
   return (
@@ -8,6 +9,22 @@ export function New() {
         showsVerticalScrollIndicator={false}
       >
         <BackButton />
+
+        <Text className="mt-6 text-white font-extrabold text-3xl">
+          Criar hábito
+        </Text>
+
+        <Text className="mt-6 text-white font-semibold text-base">
+          Qual seu comprometimento?
+        </Text>
+
+        <TextInput
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white
+          focus:border-2 focus:border-violet-600"
+          placeholder="Exercícios, dormir bem, etc..."
+          placeholderTextColor={colors.zinc[400]}
+          selectionColor={'white'}
+        />
       </ScrollView>
     </View>
   );
