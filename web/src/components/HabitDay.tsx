368d54fee0665ea493bb1fd9b0d4c7373196a64a
Mateus Jesus <mateusjdev@gmail.com>
2023-01-20 15:24:21 -0300

    feat: criando checkboxes

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,5 +1,7 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
 import * as Popover from '@radix-ui/react-popover';
 import clsx from 'clsx';
+import { Check } from 'phosphor-react';
 import { ProgressBar } from './ProgressBar';
 
 interface HabitDayProps {
@@ -29,6 +31,26 @@ export function HabitDay({ amount, completed }: HabitDayProps) {
           <span className="mt-1 font-extrabold leading-tight text-3xl">17/01</span>
 
           <ProgressBar progress={completedPercentage} />
+
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root
+              className="flex items-center gap-3 group" // adicionando a prop group
+            >
+              <div
+                className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500" // se o estado for checked, aplica o background no checkbox
+              >
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white"/>
+                </Checkbox.Indicator>
+              </div>
+
+              <span
+                className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400"
+              >
+                Beber 2L de água
+              </span>
+            </Checkbox.Root>
+          </div>
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,17 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 
 export function NewHabitForm() {
+  const availableWeekDays = [
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado',
+  ];
+
   return (
     <form className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
@@ -19,6 +30,29 @@ export function NewHabitForm() {
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+        {availableWeekDays.map(weekDay => {
+          return (
+            <Checkbox.Root
+              key={weekDay}
+              className="flex items-center gap-3 group" // adicionando a prop group
+            >
+              <div
+                className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500" // se o estado for checked, aplica o background no checkbox
+              >
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white"/>
+                </Checkbox.Indicator>
+              </div>
+
+              <span className="text-white leading-tight">
+                {weekDay}
+              </span>
+            </Checkbox.Root>
+          );
+        })}
+      </div>
 
       <button type="submit" className="mt-6 rounded-lg p-4 flex items-center
         justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
