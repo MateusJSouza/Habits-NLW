077a91c98d9ac78500322bce922acc9e3ddb4879
Mateus Jesus <mateusjdev@gmail.com>
2023-01-21 18:08:23 -0300

    fix: corrigindo o focus dos botões/checkboxes

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -62,7 +62,7 @@ export function NewHabitForm() {
         value={title}
         placeholder="ex.: Exercícios, dormir bem, etc..."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white
-        placeholder:text-zinc-400"
+        placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
         autoFocus
         onChange={(event) => setTitle(event.target.value)}
       />
@@ -76,12 +76,12 @@ export function NewHabitForm() {
           return (
             <Checkbox.Root
               key={weekDay}
-              className="flex items-center gap-3 group" // adicionando a prop group
+              className="flex items-center gap-3 group focus:outline-none" // adicionando a prop group
               checked={weekDays.includes(index)} // estará checado caso a variável weekDays inclua este índice
               onCheckedChange={() => handleToggleWeekDay(index)}
             >
               <div
-                className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500" // se o estado for checked, aplica o background no checkbox
+                className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background" // se o estado for checked, aplica o background no checkbox
               >
                 <Checkbox.Indicator>
                   <Check size={20} className="text-white"/>
@@ -96,8 +96,11 @@ export function NewHabitForm() {
         })}
       </div>
 
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center
-        justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+      <button
+        type="submit"
+        className="mt-6 rounded-lg p-4 flex items-center
+        justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500
+        transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
       >
         <Check size={20} weight="bold" />
         Confirmar
