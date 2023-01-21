646822ed454f467d7ac004c0675c939c416fb82d
Mateus Jesus <mateusjdev@gmail.com>
2023-01-21 17:45:27 -0300

    feat: sincronizando hábitos completos

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,25 +1,32 @@
 import * as Popover from '@radix-ui/react-popover';
 import clsx from 'clsx';
 import dayjs from 'dayjs';
+import { useState } from 'react';
 import { HabitsList } from './HabitsList';
 import { ProgressBar } from './ProgressBar';
 
 interface HabitDayProps {
   date: Date;
-  completed?: number;
+  defaultCompleted?: number;
   amount?: number;
 }
 
-export function HabitDay({ amount = 0, completed = 0, date }: HabitDayProps) {
+export function HabitDay({ amount = 0, defaultCompleted = 0, date }: HabitDayProps) {
+  const [completed, setCompleted] = useState(defaultCompleted);
+
   const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0;
 
   const dayAndMonth = dayjs(date).format('DD/MM');
   const dayOfWeek = dayjs(date).format('dddd');
 
+  function handleCompletedChanged(completed: number) {
+    setCompleted(completed);
+  }
+
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx('w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg', {
+        className={clsx('w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background', {
           'bg-zinc-900 border-zinc-800': completedPercentage === 0,
           'bg-violet-900 border-violet-800': completedPercentage > 0 && completedPercentage < 20,
           'bg-violet-800 border-violet-700': completedPercentage >= 20 && completedPercentage < 40,
@@ -36,7 +43,7 @@ export function HabitDay({ amount = 0, completed = 0, date }: HabitDayProps) {
 
           <ProgressBar progress={completedPercentage} />
 
-          <HabitsList date={date} />
+          <HabitsList date={date} onCompletedChanged={handleCompletedChanged} />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -5,7 +5,8 @@ import { useEffect, useState } from 'react';
 import { api } from '../lib/api';
 
 interface HabitsListProps {
-  date: Date
+  date: Date;
+  onCompletedChanged: (completed: number) => void;
 }
 
 interface HabitsInfoProps {
@@ -17,7 +18,7 @@ interface HabitsInfoProps {
   completedHabits: string[]
 }
 
-export function HabitsList({ date }: HabitsListProps) {
+export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfoProps>();
 
   useEffect(() => {
@@ -31,6 +32,30 @@ export function HabitsList({ date }: HabitsListProps) {
     });
   }, []);
 
+  async function handleToggleHabit(habitId: string) {
+    // Validando se nos hábitos completos está incluso o habitId
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId);
+
+    await api.patch(`/habits/${habitId}/toggle`);
+
+    let completedHabits: string[] = [];
+
+    if (isHabitAlreadyCompleted) {
+      // Removo da lista
+      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId);
+    } else {
+      // Adiciono na lista
+      completedHabits = [...habitsInfo!.completedHabits, habitId];
+    }
+
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits,
+    });
+
+    onCompletedChanged(completedHabits.length);
+  }
+
   const isDateInPast = dayjs(date)
     .endOf('day')
     .isBefore(new Date());
@@ -41,12 +66,13 @@ export function HabitsList({ date }: HabitsListProps) {
         return (
           <Checkbox.Root
             key={habit.id}
-            disabled={isDateInPast}
+            onCheckedChange={() => handleToggleHabit(habit.id)}
             checked={habitsInfo.completedHabits.includes(habit.id)} // quando o ID do hábito está dentro da lista dos hábitos completos, quer dizer que ele foi completado
-            className="flex items-center gap-3 group" // adicionando a prop group
+            disabled={isDateInPast}
+            className="flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed" // adicionando a prop group
           >
             <div
-              className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500" // se o estado for checked, aplica o background no checkbox
+              className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background" // se o estado for checked, aplica o background no checkbox
             >
               <Checkbox.Indicator>
                 <Check size={20} className="text-white"/>
