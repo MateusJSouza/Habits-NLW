c92fe6d5fa2b1437c93cd7c8654ecef77239cd02
Mateus Jesus <mateusjdev@gmail.com>
2023-01-21 16:42:06 -0300

    fix: borda do botão do dia atual

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -31,7 +31,7 @@ export function HabitDay({ amountOfHabits = 0, amountCompleted = 0, date, ...res
           ['bg-violet-700 border-violet-500'] : amountAccomplishedPercentage >= 40 && amountAccomplishedPercentage < 60,
           ['bg-violet-600 border-violet-500'] : amountAccomplishedPercentage >= 60 && amountAccomplishedPercentage < 80,
           ['bg-violet-500 border-violet-400'] : amountAccomplishedPercentage >= 80,
-          ['border-white border-3'] : isCurrentDay,
+          ['border-white border-4'] : isCurrentDay,
         }
       )}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
