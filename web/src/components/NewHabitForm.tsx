77073fd085096ecf58ee5a283786d4d540a00cf1
Mateus Jesus <mateusjdev@gmail.com>
2023-01-21 12:50:50 -0300

    feat: obtendo dados do formulário

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,7 +1,11 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
+import { FormEvent, useState } from 'react';
 
 export function NewHabitForm() {
+  const [title, setTitle] = useState('');
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
   const availableWeekDays = [
     'Domingo',
     'Segunda-feira',
@@ -12,8 +16,27 @@ export function NewHabitForm() {
     'Sábado',
   ];
 
+  function createNewHabit(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({title, weekDays});
+  }
+
+  function handleToggleWeekDay(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      // Percorre o array deixando no array apenas dias da semana que são diferentes do que o que eu quero remover
+      const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay);
+
+      setWeekDays(weekDaysWithRemovedOne);
+    } else {
+      const weekDaysWithAddedOne = [...weekDays, weekDay];
+
+      setWeekDays(weekDaysWithAddedOne);
+    }
+  }
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprometimento?
       </label>
@@ -21,10 +44,12 @@ export function NewHabitForm() {
       <input
         type="text"
         id="title"
+        value={title}
         placeholder="ex.: Exercícios, dormir bem, etc..."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white
         placeholder:text-zinc-400"
         autoFocus
+        onChange={(event) => setTitle(event.target.value)}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
@@ -32,11 +57,12 @@ export function NewHabitForm() {
       </label>
 
       <div className="flex flex-col gap-2 mt-3">
-        {availableWeekDays.map(weekDay => {
+        {availableWeekDays.map((weekDay, index) => {
           return (
             <Checkbox.Root
               key={weekDay}
               className="flex items-center gap-3 group" // adicionando a prop group
+              onCheckedChange={() => handleToggleWeekDay(index)}
             >
               <div
                 className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500" // se o estado for checked, aplica o background no checkbox
